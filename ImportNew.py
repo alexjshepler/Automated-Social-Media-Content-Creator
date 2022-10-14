07d64a3e9025195dfe8e199bbf9b0751fd0b00d9
Alex Shepler <alexjshepler@live.com>
2022-10-14 00:44:33 -0700

    Rewrote the entire

---
+++ ImportNew.py
@@ -0,0 +1,104 @@
+import os
+import random
+import json
+from moviepy.editor import *
+import time
+
+config = json.load(open(r'config.json'))
+
+# Video Directories
+orDir = config['videoDir']['original']
+spDir = config['videoDir']['spliced']
+
+# Screenshot Directory
+ssDir = config['screenshotDir']['screenshots']
+
+# Updates the unused quote file
+def updateQuotes():
+    # Quote Files
+    alQuotes = open(config['quoteDir']['all'], 'r')
+    unQuotes = open(config['quoteDir']['unused'], 'r+')
+    usQuotes = open(config['quoteDir']['used'], 'r+')
+
+    # Read the unused and used quotes into lists and strip of new line
+    un = unQuotes.read().splitlines()
+    us = usQuotes.read().splitlines()
+
+    # Check every quote in main quote file to see if it is in used or unused
+    for al in alQuotes.readlines():
+        if al.strip() in un or al.strip() in us:
+            pass
+        else:
+            unQuotes.writelines(format(al.strip() + '\n'))
+
+    unQuotes = open(config['quoteDir']['unused'], 'r+')
+
+    q = unQuotes.read().splitlines()
+    q = [*set(q)]
+
+    unQuotes = open(config['quoteDir']['unused'], 'w')
+    
+    for l in q:
+        unQuotes.write(format(l + '\n'))
+
+def updateVideo():
+    for v in os.listdir(orDir):
+        if v.endswith('.mp4') or v.endswith('.m4v') or v.endswith('.mkv'):
+            orLoc = format(orDir + v)
+            t = VideoFileClip(orLoc).duration
+            fType = '.mp4'
+            sEnd = 20
+            tSplice = 0
+            cSplice = 0
+            i = 0
+
+            while i + 10 < t - 10:
+                tSplice = tSplice + 1
+                i = i + 10
+
+            while sEnd < t - 10:
+                splice = VideoFileClip(orLoc).resize(height=1920).subclip(sEnd - 10, sEnd).without_audio().set_fps(30).fx(vfx.colorx, 0.5)
+                splice = splice.crop(x_center=splice.w/2, y_center=splice.h/2, width=1080, height=1920)
+
+                name = 0
+                spNames = []
+
+                for n in os.listdir(spDir):
+                    if n.endswith(fType):
+                        spNames.append(n)
+
+                while format(str(name) + fType) in spNames:
+                    name = name + 1
+                else:
+                    name = spDir + str(name) + fType
+
+                splice.write_videofile(name)
+                sEnd = sEnd + 10
+
+                splice = splice.resize(width=1080).crop(x_center=splice.w/2, y_center=splice.h/2, width=1080, height=1080)
+
+                ssNames = []
+                fType = '.png'
+                name = 0
+
+                random.seed(splice.duration * time.time())
+
+                for n in os.listdir(ssDir):
+                    if n.endswith(fType):
+                        ssNames.append(n)
+                
+                while format(str(name) + fType) in ssNames:
+                    name = name + 1
+                else:
+                    name = ssDir + str(name) + fType
+
+                splice.save_frame(name, t = int(random.randrange(int(t))))
+
+                splice.close()
+
+def shuffleAll():
+    sp = []
+
+    for n in os.listdir(spDir):
+        if n.endswith('.mp4'):
+            pass
---
+++ TODO.md
@@ -0,0 +1,14 @@
+# TODO
+
+[ ] Rewrite program
+    - [ ] Entry
+        - [ ] Load quotes
+        - [ ] Check if new videos
+        - [ ] Check if can make more content
+    - [ ] Splice Videos
+        - [ ] Take Screenshot
+    - [ ] Create Content
+        - [ ] Load all unused assets
+        - [ ] Create video
+        - [ ] Create photo
+        - [ ] Create tweet
